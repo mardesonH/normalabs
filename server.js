b458a23d63104842e68af71d497ab4425ad9d154
Mardeson <mardeson@gmail.com>
2022-07-22 23:41:16 -0300

    Feat(Criptografia de senha com sha512): Criação de encriptagem da senha para proteção

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const server = express();
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require("body-parser");
+var crypto = require('crypto');
+
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
@@ -25,7 +27,8 @@ server.get('/', (req, res) => {
     dados.email = req.body.email;
     dados.setor = req.body.setor;
     dados.cargo = req.body.cargo;
-    dados.senha = req.body.senha;
+    dados.senha = sha512(req.body.senha, gerarSalt());
+    dados.senha = dados.senha.salt.concat(dados.senha.hash);
     let token = '';
     for (var i = 80; i > 0; --i) token += (Math.floor(Math.random()*256)).toString(16);
     dados.token = token;
@@ -43,12 +46,17 @@ server.get('/', (req, res) => {
       db.all(checaNomeText, (err, result, fields) => {
       if (err) throw err;
       function checaNome(result){
-        const mesmoNome = JSON.stringify(result);
-          if (mesmoNome.length <= 0){
-            res.send('Usuário não existe')
+        let mesmoNome = JSON.stringify(result);
+        mesmoNome = mesmoNome.length - 2;
+          if (mesmoNome <= 0){
+            console.log('Usuário não existe')
+            const json = JSON.stringify(dados);
+            console.log(dados);
+            res.send(json);   
           }
           else{
-            res.send('Usuário já existe')
+            console.log('Usuário já existe')
+            res.send('false')
           };
       };
       checaNome(result);
@@ -62,21 +70,39 @@ server.get('/', (req, res) => {
 
     //console.log(checaNome);
 
-      let criar = `INSERT INTO USERS_NORMA (realUser, user, email, setor, cargo, senha, token) VALUES ( '${realUser}','${req.body.usuario}','${req.body.email}','${req.body.setor}','${req.body.cargo}','${req.body.senha}','${token}')`;
+      let criar = `INSERT INTO USERS_NORMA (realUser, user, email, setor, cargo, senha, token) VALUES ( '${realUser}','${req.body.usuario}','${req.body.email}','${req.body.setor}','${req.body.cargo}','${dados.senha}','${token}')`;
       db.run(criar);
 
-    //const json = JSON.stringify(dados);
-    //console.log(dados);
-    //res.send(json);   
-
-    
   });
+
+//Criando a criptografia para a senha
+  function gerarSalt(){
+    return  crypto.randomBytes(16).toString('hex');
+};
+
+function sha512(senha, salt){
+  var hash = crypto.createHmac('sha512', salt); // Algoritmo de cripto sha512
+  hash.update(senha);
+  var hash = hash.digest('hex');
+  return {
+      salt,
+      hash,
+  };
+};
 
   //usuario: dados[0], email: dados[1], setor: dados[2], cargo: dados[3], senha: dados[4], foto: dados[5]
 
     server.post('/login', (req, res) => {
+      //Pegar os valores do login para buscar no banco de dados
       let user = req.body.usuario;
       let senha = req.body.senha;
+      user = user.toUpperCase();
+
+      const checaNomeText = `SELECT * FROM USERS_NORMA WHERE realUser = '${user}'`;
+
+
+
+      const json = JSON.stringify(dados);
       console.log(user);
       res.end(user);
     });
